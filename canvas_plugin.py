930fad82ab4db056c80bbf41cc08e60e8318b59d
Sam O <2274654+samogden@users.noreply.github.com>
2024-06-21 10:57:59 -0700

    Cleanup

--- canvas_plugin.py
+++ canvas_plugin.py
@@ -94,29 +94,10 @@ def change_grades():
 
 
 def main():
-  canvas = canvasapi.Canvas(os.environ.get("CANVAS_API_URL"), os.environ.get("CANVAS_API_KEY"))
-  course = canvas.get_course(23751)
-  log.debug(f"{course.name}")
+  grading_flow_test()
   
   return
   
-  canvas_assignment = assignment.CanvasQuiz(quiz, course)
-  grading_helper = ai_helper.AI_Helper_fake()
-  
-  root = tk.Tk()
-  
-  menubar = tk.Menu(root)
-  file_menu = tk.Menu(menubar, tearoff=0)
-  file_menu.add_command(label="Save", command=(lambda: canvas_assignment.get_feedback()))
-  file_menu.add_command(label="Exit", command=root.quit)
-  menubar.add_cascade(label="File", menu=file_menu)
-  
-  root.config(menu=menubar)
-  
-  canvas_assignment.get_tkinter_frame(root, grading_helper)
-  root.mainloop()
-  
-
 if __name__ == "__main__":
   dotenv.load_dotenv()
   main()
