1adab7531c94db3e6f34338ec6357c87b3b9fe22
Sam O <2274654+samogden@users.noreply.github.com>
2024-09-10 09:52:20 -0700

    Removing superfluous functions

--- canvas_plugin.py
+++ canvas_plugin.py
@@ -20,21 +20,6 @@ log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
 
-def grading_flow_test():
-  # canvas = canvasapi.Canvas(os.environ.get("CANVAS_API_URL_prod"), os.environ.get("CANVAS_API_KEY_prod"))
-  course = canvas.get_course(23751)
-  log.debug(f"{course.name}")
-  
-  quiz = course.get_quiz(87942)
-  # grade_quiz(quiz, course)
-  
-  a = assignment.CanvasQuiz(quiz, course)
-  grading_helper = ai_helper.AI_Helper_fake()
-  
-  a.autograde(grading_helper)
-  
-  a.push_to_canvas(course, 87942)
-
 def GUI():
   # canvas = canvasapi.Canvas(os.environ.get("CANVAS_API_URL"), os.environ.get("CANVAS_API_KEY"))
   course = canvas.get_course(23751)
@@ -59,42 +44,6 @@ def GUI():
   canvas_assignment.get_tkinter_frame(root, grading_helper)
   root.mainloop()
   
-def change_grades():
-  # canvas = canvasapi.Canvas(os.environ.get("CANVAS_API_URL"), os.environ.get("CANVAS_API_KEY"))
-  course = canvas.get_course(23751)
-  log.debug(f"{course.name}")
-  
-  quiz = course.get_quiz(87942)
-  
-  submissions : List[canvasapi.quiz.QuizSubmission] = quiz.get_submissions()
-  quiz_questions : List[canvasapi.quiz.QuizQuestion] = quiz.get_questions()
-  log.debug(pprint.pformat(quiz_questions[0].id))
-  
-  
-  
-  for submission in submissions:
-    quiz_submission_questions : List[canvasapi.quiz.QuizSubmissionQuestion] = submission.get_submission_questions()
-    
-    
-    log.debug(f"submission: {pprint.pformat(submission.get_submission_questions())}")
-    updated_quiz = submission.update_score_and_comments(quiz_submissions=[
-      {
-        'attempt': 1,
-        'fudge_points': "null",
-        'questions': {
-          # f"{question.assessment_question_id}": {
-          f"{question.id}": {
-            'score': 10,
-            'comment': "Great job!"
-          }
-          for i, question in enumerate(quiz_submission_questions)
-        }
-      }
-    ])
-    log.debug(f"{pprint.pformat(updated_quiz.__dict__)}")
-    # submission.complete(submission.validation_token)
-
-
 def main():
   # log.debug(os.environ.get("CANVAS_API_KEY"))
   
